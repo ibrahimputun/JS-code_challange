9824092c3af9bf182f3b7eaa0f690455bf7e83dc
ibrahimputun <ibrahimputun01@gmail.com>
2023-10-28 23:56:23 +0300

    eklemeler yapıldı

--- index.js
+++ index.js
@@ -119,9 +119,97 @@ console.log(ogrenciBul("ahmet"));
 console.log(ogrenciBul("mehmet"));
 console.log(ogrenciBul("canan"));
 
+//? Örnek:7 Dizideki her bir fiyatikonsola bastiriniz.
+const prices = [250, 150, 300, 500]
 
+//? for of
+for (let price of prices) {
+    console.log(price);
+}
 
+//? for Each (tek parametreli)
+prices.forEach((p) => console.log(p * 1.1))
 
+//? 3 parametreli forEach
+
+prices.forEach((price, index) => console.log("DEGER:", price, "SIRA:", index))
+
+//? prices'ın toplamını hesaplayiniz
+let sum = 0
+prices.forEach((price) => (sum += price))
+console.log("SUM:", sum);
+
+//? 3 parametreli forEach
+//? callback func'da satir sayisi birden fazla olduğu için suslu kullandik.
+//? foreEach metodu void function'dir yani bir sey dondurmez.
+
+prices.forEach((p, i, arr) => {
+    if (p < 270) {
+        arr[i] = p * 1.1
+    }
+})
+console.log(prices);
+
+//? Örnek: 8 Bir dizideki tüm isimleri büyük harfe dönüştüren uygulamayı yaziniz.
+
+const names = ["Mustafa", "Murat", "Ahmet", "Mustafa", "Ayşe", "canan"]
+
+const upperCasedNames = names.map((n) => n.toLocaleUpperCase())
+console.log(upperCasedNames);
+console.log(names);
+
+//? Örnek 9: tlPrices dizisindeki rakamların Euro ve dolar kariliklarini hesaplatarak yeni dizelere kaydediniz.
+
+const euro = 29.8
+const dolar = 27.97
+
+const tlPrices = [100, 150, 100, 50, 80]
+const dolarPrices = tlPrices.map((tl) => Number((tl / dolar).toFixed(2)))
+console.log(dolarPrices);
+
+const euroPrices = tlPrices.map((tl) => Number((tl / euro).toFixed(2)))
+console.log(euroPrices);
+
+//? Örnek: 10 tlFiyatlar dizisindeki urunlere zam yapılmak isteniyor.Fiyatı 100 TL'den fazla olanlara %10 zam, 100 TL'den az olanlara ise %15 zam yapılmak isteniyor.Ayrıca zamlı olan yeni değerleri örnekteki gibi saklamak istiyoruz. Ürünlerin zamlı fiyatını gösteren uygulamayı yazınız.
+
+const tlFiyatlar = [100, 70, 50, 120, 200]
+
+const değerler = tlFiyatlar.map(function (d, i) {
+    if (d > 100) {
+        return `${i + 1}.Ürün Fiyatı:${(d * 1.1).toFixed(2)}`;
+    } else {
+        return `${i + 1}.Ürün Fiyatı:${(d * 1.15).toFixed(2)}`;
+    }
+
+});
+
+console.log(değerler);
+
+//? Örnek:10 Koorinatlar dizisindeki negatif koordinatları alıp yeni bir diziye saklayan uygulamayı .filter() ile yapınız.
+
+const koordinatlar = [-100, 150, -32, 43, -20]
+
+const eksiKoordinatlar = koordinatlar.filter((k) => k < 0);
+console.log(eksiKoordinatlar);
+console.log(koordinatlar);
+
+//? Örnek:11 Maasi 10000'den büyük olanları  ayrı bir diziye saklayın.
+
+const maaslar = [5500, 8000, 6500, 9000, 10000, 15000, 25000]
+
+const buyukOnbin = maaslar.filter((maas) => maas > 10000);
+
+console.log(buyukOnbin, maaslar);
+
+//? Örnek:12 Maasi 8000-10000 arasında olanları ayrı bir diziye saklayın.
+
+const aradakiMaaslar = maaslar.filter((m) => m >= 8000 && m < 10000);
+console.log(aradakiMaaslar);
+
+//? Maasi 9000'den az olanlara %50 zam yaparak değerleri yeni diziye saklayalım.
+
+const dokuzBindenAz = maaslar.filter((m) => m < 9000).map((m) => m * 1.5);
+console.log(dokuzBindenAz);
 
 
 
